1252573d7a2a59a84cf42ec7be0d0e271bd6dabe
Gokul Malik <gokulmlk21@gmail.com>
2026-04-07 00:31:40 +0530

    Db model Created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-Hello
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Hello from insde")
+})
+
+app.listen(3000, ()=>{
+    console.log("Post is running ")
+})
