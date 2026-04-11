ff4c27f776234c79d17a97243801f2ced8d1540a
Gokul Malik <gokulmlk21@gmail.com>
2026-04-11 23:49:40 +0530

    Routes added for auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,8 +7,9 @@ import {
 }from "../controllers/authController";
 import { protect } from '../middleware/auth';
 
-router.post('/register', register);
+export const register = router.post('/register', register);
 
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
+
