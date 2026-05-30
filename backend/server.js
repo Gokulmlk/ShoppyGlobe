d526685667f83050ba91f918557184c8867aa7f7
Gokul Malik <gokulmlk21@gmail.com>
2026-05-30 23:36:14 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 
 app.use(errorHandler); // Error handler middleware
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Start Server
 app.listen(PORT, () => {
